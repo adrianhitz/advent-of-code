421648770808ff0930f23be0d0e385d797a615d6
adrianhitz <32393038+adrianhitz@users.noreply.github.com>
2018-12-03 23:25:25 +0100

    Day 03 part 2

--- src/main/scala/Day03.scala
+++ src/main/scala/Day03.scala
@@ -3,25 +3,45 @@ object Day03 extends AdventIO {
 
   def main(args: Array[String]): Unit = {
     Write("03a.txt", part1.toString)
-    println(part2)
-    // Write("03b.txt", part2.toString)
+    Write("03b.txt", part2.toString)
   }
 
-  // Golf: ? bytes
+  // Golf: ? bytes TODO
   def part1(implicit s: String): Int = {
-    val ls = s.split('\n').map(_.replaceAll("(#[0-9]+ @ )| ", "")
+    val lines = s.split('\n').map(_.replaceAll("(#[0-9]+ @ )| ", "")
       .replaceAll("[:x]", ",")
       .split(',')
       .map(_.toInt)
     )
-    val a = Array.ofDim[Int](1000, 1000)
-    for(l: Array[Int] <- ls)
-      for(x <- l(0) until l(0) + l(2))
-        for(y <- l(1) until l(1) + l(3))
-          a(x)(y) += 1
-    a.flatten.count(_ >= 2)
+    val fabric = Array.ofDim[Int](1000, 1000)
+    for(line <- lines)
+      for(x <- line(0) until line(0) + line(2))
+        for(y <- line(1) until line(1) + line(3))
+          fabric(x)(y) += 1
+    fabric.flatten.count(_ >= 2)
   }
 
-  // Golf: ? bytes
-  def part2(implicit s: String): String = ???
+  // Golf: ? bytes TODO
+  def part2(implicit s: String): Int = {
+    val lines = s.split('\n').map(_.replaceAll("[ #]", "")
+      .replaceAll("[@:x]", ",")
+      .split(',')
+      .map(_.toInt)
+    )
+    val fabric = Array.ofDim[Int](1000, 1000)
+    for(line <- lines)
+      for(x <- line(1) until line(1) + line(3))
+        for(y <- line(2) until line(2) + line(4))
+          fabric(x)(y) += 1
+    fabric.flatten.count(_ >= 2)
+    for(line <- lines) {
+      val sliced = fabric.slice(line(1), line(1) + line(3)).map(_.slice(line(2), line(2) + line(4)))
+      var overlap = false
+      for(i <- sliced.flatten) {
+        if(i != 1) overlap = true
+      }
+      if(!overlap) return line(0)
+    }
+    throw new IllegalArgumentException("No solution")
+  }
 }
