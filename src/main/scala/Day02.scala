3242ac987a83c20e87ca06a555131da07bea9e9e
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-12-03 21:56:26 +0100

    Day 02 part 2

--- src/main/scala/Day02.scala
+++ src/main/scala/Day02.scala
@@ -1,11 +1,9 @@
-
 object Day02 extends AdventIO {
-
   implicit val s: String = Read("02.txt")
 
   def main(args: Array[String]): Unit = {
     Write("02a.txt", part1.toString)
-    // Write("02b.txt", part2.toString)
+    Write("02b.txt", part2.toString)
   }
 
   // Golf: 133 bytes
@@ -13,13 +11,22 @@ object Day02 extends AdventIO {
     var a, b = 0
     for(l <- s.split('\n')) {
       def f(x: Int) = l.foldLeft(false)((i, c) => if(l.count(_ == c) == x) true else i)
+
       if(f(2)) a += 1
       if(f(3)) b += 1
     }
     a * b
   }
 
-  // Golf:
-  def part2(implicit s: String): Int = ???
-
+  // Golf: 112 bytes
+  def part2(implicit s: String): String = {
+    val t = s.split('\n')
+    for(a <- t)
+      for(b <- t) {
+        var s = ""
+        for(i <- 0 to 25) if(a(i) == b(i)) s += a(i)
+        if(s.length == 25) return s
+      }
+    s
+  }
 }
