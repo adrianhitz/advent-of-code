2025854cce7c4ee60d51a9ce5ad7f1dd4a3dcc46
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-12-10 10:09:55 +0100

    Day 10 cleanup

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -6,12 +6,87 @@ object Day10 extends AdventIO {
     Write("10b.txt", part2.toString)
   }
 
+  // Golf: ? bytes TODO
+  def part1(implicit s: String): String = {
+    val v: Array[Array[Int]] = s.split('\n').map(x => Array(
+      x.substring(10, 16), // position x
+      x.substring(17, 24), // position y
+      x.substring(36, 38), // velocity x
+      x.substring(39, 42) // velocity y
+    ).map(_.trim.toInt))
+    var sb = new StringBuilder("No solution found")
+    for(t <- 0 to 100000) {
+      val points = new Array[(Int, Int)](v.length)
+      for((l: Array[Int], i: Int) <- v.zipWithIndex) {
+        points(i) = (l(0) + t * l(2), l(1) + t * l(3))
+      }
+      val xRange = (points.minBy(_._1)._1, points.maxBy(_._1)._1)
+      val yRange = (points.minBy(_._2)._2, points.maxBy(_._2)._2)
+      val width = xRange._2 - xRange._1
+      val height = yRange._2 - yRange._1
+      if(width <= 100 && height <= 10) {
+        val pixels = Array.ofDim[Boolean](width + 1, height + 1)
+        for(point <- points) {
+          pixels(point._1 - xRange._1)(point._2 - yRange._1) = true
+        }
+        sb = new StringBuilder
+        for(y <- pixels(0).indices) {
+          for(x <- pixels.indices) {
+            if(pixels(x)(y)) sb.append('#') else sb.append('.')
+          }
+          sb.append('\n')
+        }
+      }
+    }
+    sb.toString
+  }
+
+  // Golf: ? bytes TODO
+  def part2(implicit s: String): Int = {
+    val v: Array[Array[Int]] = s.split('\n').map(x => Array(
+      x.substring(10, 16), // position x
+      x.substring(17, 24), // position y
+      x.substring(36, 38), // velocity x
+      x.substring(39, 42) // velocity y
+    ).map(_.trim.toInt))
+    val tMax = 100000
+    var tWhenFound = tMax
+    var sb = new StringBuilder("No solution found")
+    for(t <- 0 to tMax) {
+      val points = new Array[(Int, Int)](v.length)
+      for((l: Array[Int], i: Int) <- v.zipWithIndex) {
+        points(i) = (l(0) + t * l(2), l(1) + t * l(3))
+      }
+      val xRange = (points.minBy(_._1)._1, points.maxBy(_._1)._1)
+      val yRange = (points.minBy(_._2)._2, points.maxBy(_._2)._2)
+      val width = xRange._2 - xRange._1
+      val height = yRange._2 - yRange._1
+      if(width <= 100 && height <= 10) {
+        val pixels = Array.ofDim[Boolean](width + 1, height + 1)
+        for(point <- points) {
+          pixels(point._1 - xRange._1)(point._2 - yRange._1) = true
+        }
+        sb = new StringBuilder
+        for(y <- pixels(0).indices) {
+          for(x <- pixels.indices) {
+            if(pixels(x)(y)) sb.append('#') else sb.append('.')
+          }
+          sb.append('\n')
+        }
+        tWhenFound = t
+      }
+    }
+    tWhenFound
+
+
+  }
+
   def solve(s: String): (String, Int) = {
     val v: Array[Array[Int]] = s.split('\n').map(x => Array(
-      x.substring(10, 16),
-      x.substring(17, 24),
-      x.substring(36, 38),
-      x.substring(39, 42)
+      x.substring(10, 16), // position x
+      x.substring(17, 24), // position y
+      x.substring(36, 38), // velocity x
+      x.substring(39, 42) // velocity y
     ).map(_.trim.toInt))
     var t = 0
     val tMax = 100000
@@ -34,10 +109,7 @@ object Day10 extends AdventIO {
         sb = new StringBuilder
         for(y <- pixels(0).indices) {
           for(x <- pixels.indices) {
-            if(pixels(x)(y))
-              sb.append('#')
-            else
-              sb.append('.')
+            if(pixels(x)(y)) sb.append('#') else sb.append('.')
           }
           sb.append('\n')
         }
@@ -47,10 +119,5 @@ object Day10 extends AdventIO {
     }
     (sb.toString, tWhenFound)
   }
-  // Golf: ? bytes TODO
-  def part1(implicit s: String): String = solve(s)._1
-
-  // Golf: ? bytes TODO
-  def part2(implicit s: String): Int = solve(s)._2
 
 }
