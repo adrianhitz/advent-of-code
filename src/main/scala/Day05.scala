4113fb9db1f3af92c4ac0f69c0807b1327986a5f
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-12-10 10:46:41 +0100

    Day 05 part 2

--- src/main/scala/Day05.scala
+++ src/main/scala/Day05.scala
@@ -3,7 +3,7 @@ object Day05 extends AdventIO {
 
   def main(args: Array[String]): Unit = {
     Write("05a.txt", part1.toString)
-    // Write("05b.txt", part2.toString)
+    Write("05b.txt", part2.toString)
   }
 
   // Golf: 93 bytes
@@ -17,7 +17,19 @@ object Day05 extends AdventIO {
     l
   }
 
-  // Golf: ? bytes TODO
-  def part2(implicit s: String): Unit = ???
+  // Golf: 202 bytes
+  def part2(implicit s: String): Int = {
+    var m = 99999
+    for(i <- 'A'.toInt to 'Z'.toInt) {
+      var t = s.replaceAll("[" + i.toChar + "" + (i + 32).toChar + "]", "")
+      var l = 99999
+      while(t.length < l) {
+        l = t.length
+        t = t.replaceAll("(\\w)(?!\\1)(?i:\\1)", "")
+      }
+      m = Math.min(m, l)
+    }
+    m
+  }
 
 }
