04f5ad21f6e8842cf9c9664a3a18be430dca08f2
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-12-11 16:09:37 +0100

    Day 07 part 2

--- src/main/scala/Day07.scala
+++ src/main/scala/Day07.scala
@@ -6,7 +6,7 @@ object Day07 extends AdventIO {
 
   def main(args: Array[String]): Unit = {
     Write("07a.txt", part1)
-    // Write("07b.txt", part2.toString)
+    Write("07b.txt", part2.toString)
   }
 
   // Golf: ? bytes TODO
@@ -32,5 +32,43 @@ object Day07 extends AdventIO {
   }
 
   // Golf: ? bytes TODO
-  def part2(implicit s: String): Unit = ???
+  def part2(implicit s: String): Int = {
+    val reqList: Array[(Char, Char)] = s.split('\n').map(x => (x.charAt(5), x.charAt(36)))
+    val reqs = mutable.Map[Char, Set[Char]]() // Map containing all required steps for a given step
+    for(req <- reqList) {
+      if(!reqs.contains(req._2)) reqs(req._2) = Set()
+      reqs(req._2) = reqs(req._2) + req._1
+    }
+    var nextSteps = reqs.values.flatten.toSet diff reqs.keys.toSet
+    val completed = ListBuffer[Char]()
+    case class Worker(step: Char, started: Int, duration: Int)
+    var workers = List[Worker]()
+    var t = -1
+    while(nextSteps.nonEmpty || workers.nonEmpty) {
+      t += 1
+      // Complete work
+      var finished = List[Worker]()
+      for(worker <- workers) {
+        if(worker.started + worker.duration <= t) {
+          completed.append(worker.step)
+          finished :+= worker
+        }
+        workers = workers diff finished
+      }
+      completed.foreach(reqs.remove)
+      // Find available steps
+      for((k, v) <- reqs) {
+        if(v.subsetOf(completed.toSet)) nextSteps += k
+      }
+      nextSteps = nextSteps diff completed.toSet
+      // Assign work
+      while(nextSteps.nonEmpty && workers.length < 5) {
+        val nextStep = nextSteps.min
+        workers :+= Worker(nextStep, t, nextStep - 'A' + 61)
+        nextSteps -= nextStep
+        reqs.remove(nextStep)
+      }
+    }
+    t
+  }
 }
