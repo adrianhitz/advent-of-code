8aafbf99ece23c002eb6eafa73c2be2a2a18041d
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2018-12-11 12:38:20 +0100

    Day 11 improve readability

--- src/main/scala/Day11.scala
+++ src/main/scala/Day11.scala
@@ -9,16 +9,16 @@ object Day11 extends AdventIO {
   // Golf: ? bytes TODO
   def part1(implicit s: String): String = {
     val serial = s.toInt
-    val grid = Array.ofDim[Int](300, 300)
+    val grid = Array.ofDim[Int](301, 301)
     for(x <- 1 to 300) for(y <- 1 to 300) {
-      grid(x - 1)(y - 1) = ((((x + 10) * y + serial) * (x + 10)) / 100) % 10 - 5
+      grid(x)(y) = ((((x + 10) * y + serial) * (x + 10)) / 100) % 10 - 5
     }
-    var max = 0
+    var maxSum = 0
     var maxCoords = (0, 0)
     for(x <- 1 to 298) for(y <- 1 to 298) {
-      val v = grid.slice(x - 1, x + 2).flatMap(_.slice(y - 1, y + 2)).sum
-      if(v > max) {
-        max = v
+      val sum = grid.slice(x, x + 3).flatMap(_.slice(y, y + 3)).sum
+      if(sum > maxSum) {
+        maxSum = sum
         maxCoords = (x, y)
       }
     }
@@ -41,12 +41,12 @@ object Day11 extends AdventIO {
     var maxSize = 0
     for(n <- 1 to 300) {
       for(x <- 1 to (301 - n)) for(y <- 1 to (301 - n)) {
-        val l = sums(x - 1)(y + (n - 1))
-        val t = sums(x + (n - 1))(y - 1)
-        val tl = sums(x - 1)(y - 1)
-        val v = sums(x + (n - 1))(y + (n - 1)) - l - t + tl
-        if(v > maxSum) {
-          maxSum = v
+        val left = sums(x - 1)(y + (n - 1))
+        val top = sums(x + (n - 1))(y - 1)
+        val topLeft = sums(x - 1)(y - 1)
+        val sum = sums(x + (n - 1))(y + (n - 1)) - left - top + topLeft
+        if(sum > maxSum) {
+          maxSum = sum
           maxCoords = (x, y)
           maxSize = n
         }
